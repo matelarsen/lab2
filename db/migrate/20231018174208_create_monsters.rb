2ea0b86b23701b8f6141846fa4ee360c98203aa5
larse <larsenmateo.ml@gmail.com>
2023-10-18 17:51:41 -0300

    Creacion Clases

--- db/migrate/20231018174208_create_monsters.rb
+++ db/migrate/20231018174208_create_monsters.rb
@@ -1,9 +1,12 @@
 class CreateMonsters < ActiveRecord::Migration[7.0]
   def change
     create_table :monsters do |t|
+      t.integer :scare_level
       t.string :name
       t.text :description
-      t.integer :scare_level
+      t.datetime :created_at
+      t.datetime :updated_at
+
       t.timestamps
     end
   end
